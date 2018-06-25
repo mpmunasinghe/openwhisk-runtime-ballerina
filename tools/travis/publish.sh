3d9f980d1ccc7a08a1e8080fc808a9be916d9135
mpmunasinghe <mpmunasinghe@gmail.com>
2018-06-25 15:16:58 +0530

    removing the RUNTIME version resolution

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -33,10 +33,8 @@ if [[ ! -z ${DOCKER_USER} ]] && [[ ! -z ${DOCKER_PASSWORD} ]]; then
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 fi
 
-if [[ ! -z ${RUNTIME} ]]; then
 TERM=dumb ./gradlew \
 :ballerina:distDocker \
 -PdockerRegistry=docker.io \
 -PdockerImagePrefix=${IMAGE_PREFIX} \
 -PdockerImageTag=${IMAGE_TAG}
-fi
